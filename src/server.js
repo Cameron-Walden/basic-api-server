d9129d9a9aee295a79fe24ce011c6b6a8173a4d9
Cameron <cameronkwalden@gmail.com>
2021-10-21 12:23:18 -0700

    uncomments code

--- src/server.js
+++ src/server.js
@@ -7,25 +7,25 @@ app.use(express.json());
 
 
 //comment all of this out to get tests to run
-// const notFoundErrorHandler = require('./error-handlers/404.js');
-// const unexpectedErrorHandler = require('./error-handlers/500.js');
+const notFoundErrorHandler = require('./error-handlers/404.js');
+const unexpectedErrorHandler = require('./error-handlers/500.js');
 
-// const bookRoute = require('./routes/book.js');
-// const movieRoute = require('./routes/movie.js');
+const bookRoute = require('./routes/book.js');
+const movieRoute = require('./routes/movie.js');
 
-// const logger = require('./middleware/logger.js')
+const logger = require('./middleware/logger.js')
 
-// app.get('/', (request, response) => {
-//   response.status(200).send('This is a test route');
-// });
+app.get('/', (request, response) => {
+  response.status(200).send('This is a test route');
+});
 
-// app.use('/book', bookRoute);
-// app.use('/movie', movieRoute);
+app.use('/book', bookRoute);
+app.use('/movie', movieRoute);
 
-// app.use('*', notFoundErrorHandler);
-// app.use(unexpectedErrorHandler);
+app.use('*', notFoundErrorHandler);
+app.use(unexpectedErrorHandler);
 
-// app.use(logger);
+app.use(logger);
 
 module.exports = {
   server: app,
